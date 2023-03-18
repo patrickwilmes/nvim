1a2ed2d75a7cf28e7a972ce85559e04ca818e014
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2023-03-18 11:45:21 +0100

    Adding go support
    Adding gopls as language server to mason, for go support .

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,6 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = { "lua_ls", "clangd", "ltex", "marksman", "pylsp", "cmake", "html", "jsonls" }
+  ensure_installed = { "lua_ls", "clangd", "ltex", "marksman", "pylsp", "cmake", "html", "jsonls", "gopls" }
 })
 
 local on_attach = function(_, _)
@@ -35,5 +35,8 @@ require("lspconfig").html.setup {
   on_attach = on_attach
 }
 require("lspconfig").jsonls.setup {
+  on_attach = on_attach
+}
+require("lspconfig").gopls.setup {
   on_attach = on_attach
 }
